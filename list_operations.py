b8e1f042418c01d1afa3141264d4507f595570e3
Hackbright Student <info@hackbrightacademy.com>
2016-07-08 12:31:29 -0700

    Finished remaining doctests, passed

--- list_operations.py
+++ list_operations.py
@@ -91,7 +91,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -105,7 +105,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -119,7 +119,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -133,7 +133,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -148,7 +148,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2:1]
 
 
 def replace_head(input_list):
@@ -164,7 +164,7 @@ def replace_head(input_list):
     True
 
     """
-
+    input_list[0] = 42
     pass
 
 
@@ -181,7 +181,8 @@ def replace_third_and_last(input_list):
     True
 
     """
-
+    input_list[2] = 37
+    input_list[-1] = 37
     pass
 
 
@@ -199,7 +200,8 @@ def replace_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2] = [42, 37]
+
 
 
 def delete_third_and_seventh(input_list):
@@ -215,8 +217,8 @@ def delete_third_and_seventh(input_list):
     True
 
     """
-
-    pass
+    del input_list[2]
+    del input_list[5]
 
 
 def delete_middle(input_list):
@@ -233,7 +235,8 @@ def delete_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2] = []
+
 
 
 ##############################################################################
